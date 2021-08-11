ddc7aece59de78e6c94ca64820e1d854062825c6
aleksq7 <morrisalex2001@gmail.com>
2021-08-11 10:28:09 -0600

    commit tree ebb64ec2ae8d59dba310b5033da74952e71b4d03

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(index){
+    let cardList = [];
+    for (let name of index){
+        cardList.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+    }
+    return cardList;
+}
+
+function countDown(num){
+    for (let i = num; i > -1; i--)
+    console.log(i);
+}
